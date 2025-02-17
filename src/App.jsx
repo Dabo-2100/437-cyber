b8ad82716eee204a8dfb234bcaadadfeeb432218
Dabo-2100 <a_fattah_m@icloud.com>
2025-02-17 18:45:56 +0200

    First

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello This is My App</h1>
+    </div>
+  )
+}
+
+// installing the App using vite
+// npm i bootstrap animate.css axios react-router-dom react-icons sweetalert2
+// JSX - Component - Project Structure (Node - Node Modules - How Run The Project)
+// Page ??  
